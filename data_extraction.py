6be29bc8c2795f07706b5aceb5ff04cd9e9f6aaf
Pavan <rapolu.pavann@gmail.com>
2023-12-25 19:36:58 -0600

    Update data_extraction.py
    Used 'zip' to combine headers and row data into a dictionary for DataFrame insertion.
    Combined 'network_data' DataFrame update into a single step to improve efficiency.
    Used constant 'BASE_URL' for the base URL instead of repeating it.

--- data_extraction.py
+++ data_extraction.py
@@ -3,57 +3,48 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from tqdm import tqdm
 
-url = 'https://www.bitcoin-otc.com/viewratings.php'
-page = requests.get(url)
+BASE_URL = 'https://www.bitcoin-otc.com/'
 
-soup = BeautifulSoup(page.text, 'lxml')
-table1 = soup.find("table", {"class":"datadisplay sortable"})
+def get_soup(url):
+    page = requests.get(url)
+    return BeautifulSoup(page.text, 'lxml')
 
-headers = []
-for i in table1.find_all("th"):
- title = i.text
- headers.append(title)
+def extract_table_headers(table):
+    return [th.text for th in table.find_all("th")]
 
+def extract_inner_table_headers(inner_table):
+    return [th.text for th in inner_table.find_all("th")]
+
+def extract_table_data(table, headers, data_frame):
+    for row in tqdm(table.find_all("tr")[1:]):
+        row_data = [td.text for td in row.find_all("td")]
+        inner_url = BASE_URL + row.find_all("td")[1].a['href']
+        inner_table = get_soup(inner_url).find("table", {"class": "datadisplay sortable"})
+        for inner_row in inner_table.find_all("tr")[1:]:
+            inner_row_data = [td.text for td in inner_row.find_all("td")]
+            data_frame = data_frame.append(dict(zip(headers + extract_inner_table_headers(inner_table), row_data + inner_row_data)), ignore_index=True)
+    return data_frame
+
+url = BASE_URL + 'viewratings.php'
+soup = get_soup(url)
+table1 = soup.find("table", {"class": "datadisplay sortable"})
+
+headers = extract_table_headers(table1)
 print(headers)
 
-first_row = table1.find_all("tr")[1]
-first_tds = first_row.find_all("td")
-first_url = 'https://www.bitcoin-otc.com/' + first_tds[1].a['href'];
-first_page = requests.get(first_url)
-first_soup = BeautifulSoup(first_page.text,'lxml')
-first_table = first_soup.find("table", {"class":"datadisplay sortable"})
+node_data = pd.DataFrame(columns=headers)
+network_data = pd.DataFrame()
 
-inner_headers = []
-for i in first_table.find_all("th"):
-  title = i.text
-  inner_headers.append(title)
-
-print(inner_headers)
-
-node_data = pd.DataFrame(columns = headers)
-network_data = pd.DataFrame(columns = inner_headers)
-
-
-for j in tqdm(table1.find_all("tr")[1:]):
- row_data = j.find_all("td")
- row = [i.text for i in row_data]
- inner_url = 'https://www.bitcoin-otc.com/' + row_data[1].a['href']
- inner_page = requests.get(inner_url)
- inner_soup = BeautifulSoup(inner_page.text,'lxml')
- inner_table = inner_soup.find("table", {"class":"datadisplay sortable"})
- for k in inner_table.find_all("tr")[1:]:
-   inner_row_data = k.find_all("td")
-   #print(len(inner_row_data))
-   inner_row = [l.text for l in inner_row_data]
-   inner_length = len(network_data)
-   network_data.loc[inner_length] = inner_row
-
- length = len(node_data)
- node_data.loc[length] = row
+for row in tqdm(table1.find_all("tr")[1:]):
+    row_data = [td.text for td in row.find_all("td")]
+    inner_url = BASE_URL + row.find_all("td")[1].a['href']
+    inner_table = get_soup(inner_url).find("table", {"class": "datadisplay sortable"})
+    network_data = extract_table_data(inner_table, headers, network_data)
+    node_data = node_data.append(dict(zip(headers, row_data)), ignore_index=True)
 
 print(node_data.head(5))
 print()
 print(network_data.head(5))
 
-node_data.to_csv("nodes.csv",index=False)
-network_data.to_csv("network.csv",index=False)
+node_data.to_csv("nodes.csv", index=False)
+network_data.to_csv("network.csv", index=False)
